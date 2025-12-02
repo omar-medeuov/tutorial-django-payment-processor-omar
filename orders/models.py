3913bb36910c00e049f2e61ee2d6fe6a019720b3
omar-medeuov <omarmedeuov@gmail.com>
2025-12-02 14:47:24 +0500

    clean up models, urls, views

--- orders/models.py
+++ orders/models.py
@@ -1,12 +1,1 @@
-import random
 from django.db import models
-
-
-class Order(models.Model):
-    secret = models.CharField(max_length=1000, default="", blank=True)
-    amount = models.DecimalField(default=0)
-    paid = models.BooleanField(default=False)
-    checkout_url = models.CharField(max_length=1000, default="", blank=True)
-
-    def generate_secret(self):
-        self.secret = str(random.randint(10000, 99999))
--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("confirm/<int:order_id>/<str:order_secret>", views.confirm),
-    path("orders/<int:order_id>", views.order),
+
 ]
